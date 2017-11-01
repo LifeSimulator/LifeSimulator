25a4da9eae7205fec8d98b2f90e46d567672c88e
10shibu <shibu@brandeis.edu>
2017-11-01 19:22:48 -0400

    homepage with buttons and intent to userprofile page

---
+++ app/src/main/java/subversion_team/cs/brandeis/edu/lifesimulator/UserProfile.java
@@ -0,0 +1,17 @@
+package subversion_team.cs.brandeis.edu.lifesimulator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xichen on 11/1/17.
+ */
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
